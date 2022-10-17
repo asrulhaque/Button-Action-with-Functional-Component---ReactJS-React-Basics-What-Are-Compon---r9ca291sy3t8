3c5cce19bccdb00d9a5084b79c39460be45f252d
Asrul Haque <asrulhaque123@gmail.com>
2022-10-18 02:10:32 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-function App() {
+function App() 
+{
+  const [displayMessage, setDisplayMessage] = useState(false);
+
+  
   return (
-    <div id="main">
-      // Do not alter the main div
+	<div id="main">
+		{
+    		(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    	}
+		<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
     </div>
   );
 }
